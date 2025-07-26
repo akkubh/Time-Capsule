833ad8af6da6c9153060e2cb08a023b09a05eb3c
Aishwarya Majjagi <aishwaryamajjagi1215@gmail.com>
2025-07-26 19:05:20 +0530

    login/signup added

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 import sqlite3
 import random
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key ='highly_secure_uncrackable_key_no_one_can_guess'
 
 #return messages
 NOTES = [
@@ -20,10 +21,56 @@ def get_db():
     conn.row_factory = sqlite3.Row
     return conn
 
+#signup
+@app.route("/signup", methods = ["POST"])
+def signup():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return jsonify({"error": "Username and Password required"}), 400
+
+    conn = get_db()
+    cur = conn.cursor()
+
+    try:
+        cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        return jsonify({"message": "Signup successful!"})
+    except sqlite3.IntegrityError:
+        return jsonify({"error": "Username already exists"}), 409
+    finally:
+        conn.close()
+
+#logging in
+@app.route("/login", methods = ["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = cur.fetchone()
+
+    if user:
+        session["user_id"] = user["username"]
+        return jsonify({"message": "Login Successful!"})
+    else:
+        return jsonify("error": "Invalid login"), 401
+
+#logout
+@app.route("/logout", methods = ["POST"])
+def logout():
+    session.pop("user_id", None)
+    return jsonify({"message": "Logged out"})
+
 #creating capsules
 @app.route("/create_capsule", methods = ["POST"])
 def create_capsule():
-    if user_id not in session:
+    if "user_id" not in session:
         return jsonify({"error": "Login Required"}), 401
 
     data = request.json
@@ -39,12 +86,12 @@ def create_capsule():
 
     conn = get_db()
     cur = conn.cursor()
-    cur.execute("""INSERT INTO messages (message, ulock_date_time, mood, creation_date, user_id) VALUES(?, ?, ?, ?)""", (message, unlock_date_time, mood, creation_date, user_id))
+    cur.execute("""INSERT INTO messages (message, unlock_date_time, mood, creation_date, user_id) VALUES(?, ?, ?, ?, ?)""", (message, unlock_date_time, mood, creation_date, user_id))
     conn.commit()
     conn.close()
 
     return jsonify({
-        "status" = "message_saved",
-        "note" = random.choice(NOTES),
-        "creation" = creation_date
+        "status": "message_saved",
+        "note": random.choice(NOTES),
+        "creation": creation_date
     })
