4da83f9a1d1a5216d960b308ee86db44e6ee0306
akkubh <ankitabhargava771@gmail.com>
2025-07-28 03:57:56 +0530

    js added

---
+++ script.js
@@ -0,0 +1,25 @@
+function openPopup(id) {
+    document.getElementById(id).style.display = 'flex';
+  }
+
+  function closePopup(id) {
+    document.getElementById(id).style.display = 'none';
+  }
+  }
+  
+  const faqButtons = document.querySelectorAll('.faq-question');
+
+  faqButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const answer = btn.nextElementSibling;
+      const isOpen = btn.classList.contains('active');
+
+      document.querySelectorAll('.faq-answer').forEach(a => a.style.display = 'none');
+      document.querySelectorAll('.faq-question').forEach(q => q.classList.remove('active'));
+
+      if (!isOpen) {
+        answer.style.display = 'block';
+        btn.classList.add('active');
+      }
+    });
+  });
